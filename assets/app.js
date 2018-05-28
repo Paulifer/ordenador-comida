7fe3f465e16699319194baa4b68fe160de9414af
Paulifer <pauli.hua.timm@gmail.com>
2018-05-28 01:38:23 -0400

    tratando de hacer funcionar el click con add.event.listener, pero no funciona

--- assets/app.js
+++ assets/app.js
@@ -15,3 +15,19 @@ food = document.getElementById('comida').value;
 			alert('justo queda la ultima, buen provecho');
 		}
 	}*/
+/* intento por llamar a boton y ejectutar la funcion
+
+
+const btn = document.getElementById('btnOk').value;
+	btn.addEventListener('click', function(){
+	if (comida === 'ceviche'){
+			alert("oouchh, lo sentimos se acabo");
+		}else if(comida === 'tacos al pastor'){
+			alert('lo sentimos, se los comieron todos');
+		}else if(comida === 'empanada de pino'){ 
+			alert('El guaton loyola se ha llevado la ultima');
+		}else  if (comida === 'hamburgueza'){
+			alert('justo queda la ultima, buen provecho');
+		};
+	})
+	*/
