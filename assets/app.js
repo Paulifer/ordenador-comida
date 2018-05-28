7b4955e6d9db1d5c0cba1d7dd34c283d2a9d20a1
Paulifer <pauli.hua.timm@gmail.com>
2018-05-28 01:36:06 -0400

    haciendo funcion arrow he implementado con el input en html, pero no me funciona

---
+++ assets/app.js
@@ -0,0 +1,17 @@
+
+
+
+
+/*tratando de hacer funcionar con boton function arrow
+const food = (comida) =>{
+food = document.getElementById('comida').value;
+		if (comida === 'ceviche'){
+			alert("oouchh, lo sentimos se acabo");
+		}else if(comida === 'tacos al pastor'){
+			alert('lo sentimos, se los comieron todos');
+		}else if(comida === 'empanada de pino'){ 
+			alert('El guaton loyola se ha llevado la ultima');
+	}else  if (comida === 'hamburgueza'){
+			alert('justo queda la ultima, buen provecho');
+		}
+	}*/
